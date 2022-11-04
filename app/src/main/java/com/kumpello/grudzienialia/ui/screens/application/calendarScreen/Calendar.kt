1d2079db8f5566ff3a866d38a59dcbd961ed2de9
Kumpel <bleidd04@gmail.com>
2022-11-04 17:40:43 +0100

    -adds calendar dependency

--- app/src/main/java/com/kumpello/grudzienialia/ui/screens/application/calendarScreen/Calendar.kt
+++ app/src/main/java/com/kumpello/grudzienialia/ui/screens/application/calendarScreen/Calendar.kt
@@ -1,35 +1,39 @@
 package com.kumpello.grudzienialia.ui.screens.application.calendarScreen
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
-import com.kumpello.grudzienialia.R
+import androidx.compose.ui.tooling.preview.Preview
+import com.himanshoe.kalendar.Kalendar
+import com.himanshoe.kalendar.model.KalendarType
 
 @Composable
 fun Calendar() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700))
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "Home Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
-    }
+    Scaffold(content = { padding ->
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
+            Kalendar(kalendarType = KalendarType.Firey)
+        }
+    })
+}
+
+@Composable
+fun SelectedDay() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalendarPreview() {
+    Calendar()
 }
