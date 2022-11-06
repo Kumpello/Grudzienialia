c54d0624362cbe6854640a7794e71337ceddce7f
Kumpel <bleidd04@gmail.com>
2022-11-06 20:33:13 +0100

    -friends composable wip

--- app/src/main/java/com/kumpello/grudzienialia/ui/screens/application/friendsScreen/Friends.kt
+++ app/src/main/java/com/kumpello/grudzienialia/ui/screens/application/friendsScreen/Friends.kt
@@ -1,43 +1,119 @@
 package com.kumpello.grudzienialia.ui.screens.application.friendsScreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.kumpello.grudzienialia.R
-import com.kumpello.grudzienialia.ui.screens.application.calendarScreen.Calendar
+import com.kumpello.grudzienialia.domain.model.NetworkModule
+import com.kumpello.grudzienialia.domain.usecase.FriendsService
+import com.kumpello.grudzienialia.ui.theme.GrudzienialiaTheme
 
 @Composable
-fun Friends() {
+fun Friends(friendsService: FriendsService) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.teal_700))
             .wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Notification Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        Row() {
+            
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(colorResource(id = R.color.teal_700))
+                .wrapContentSize(Alignment.Center)
+        ) {
+
+        }
+    }
+
+}
+
+@Composable
+fun AddFriendButton() {
+    Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+        Button(
+            onClick = {
+
+                },
+            shape = RoundedCornerShape(50.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Text(text = "Add Friend")
+        }
+    }
+}
+
+@Composable
+fun makeAlertDialog(text: String, callback: (Result<Unit>) -> Unit) {
+    GrudzienialiaTheme() {
+        Column {
+            val openDialog = remember { mutableStateOf(false)  }
+
+            Button(onClick = {
+                openDialog.value = true
+            }) {
+                Text("Click me")
+            }
+
+            if (openDialog.value) {
+
+                AlertDialog(
+                    onDismissRequest = {
+                        // Dismiss the dialog when the user clicks outside the dialog or on the back
+                        // button. If you want to disable that functionality, simply use an empty
+                        // onCloseRequest.
+                        openDialog.value = false
+                    },
+                    title = {
+                        Text(text = "Dialog Title")
+                    },
+                    text = {
+                        Text("Here is a text ")
+                    },
+                    confirmButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the Confirm Button")
+                        }
+                    },
+                    dismissButton = {
+                        Button(
+
+                            onClick = {
+                                openDialog.value = false
+                            }) {
+                            Text("This is the dismiss Button")
+                        }
+                    }
+                )
+            }
+        }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun FriendsPreview() {
-    Friends()
+    Friends(NetworkModule.provideFriendsService())
 }
